adccb37935fc5fdba737b26d908f4986f9596f46
David Eisinger <david.eisinger@gmail.com>
2021-06-06 16:44:12 -0400

    Add signature w/ basic check

--- schema.sql
+++ schema.sql
@@ -5,6 +5,9 @@ CREATE TABLE bookmarks (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   url TEXT NOT NULL UNIQUE,
   parent_id INTEGER,
+  signature TEXT NOT NULL CHECK(
+    signature = sha1(url)
+  ),
   FOREIGN KEY(parent_id) REFERENCES bookmarks(id)
 );
 
@@ -12,7 +15,7 @@ CREATE UNIQUE INDEX parent_unique ON bookmarks (
   ifnull(parent_id, 0)
 );
 
-INSERT INTO bookmarks (url) VALUES ("google");
-INSERT INTO bookmarks (url, parent_id) VALUES ("yahoo", 1);
-INSERT INTO bookmarks (url, parent_id) VALUES ("bing", 2);
-INSERT INTO bookmarks (url, parent_id) VALUES ("duckduckgo", 3);
+INSERT INTO bookmarks (url, signature) VALUES ("google", sha1("google"));
+INSERT INTO bookmarks (url, signature, parent_id) VALUES ("yahoo", sha1("yahoo") , 1);
+INSERT INTO bookmarks (url, signature, parent_id) VALUES ("bing", sha1("bing"), 2);
+INSERT INTO bookmarks (url, signature, parent_id) VALUES ("duckduckgo", sha1("duckduckgo"), 3);
